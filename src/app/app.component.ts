568f209164c3a1c3a7ae8391d4e28ed50d359d7d
Ralph Küpper <ralph.kuepper@skelpo.com>
2023-01-12 12:06:13 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {
+    
+  }
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event) => {
+      let e: any = event;
+      gtag('event', 'page_view', {
+        page_path: e.urlAfterRedirects
+      })
+    })
+  }
   title = 'metaradar';
+
+  
 }
